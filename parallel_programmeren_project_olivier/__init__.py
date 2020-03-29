bc9d3edab538095364fd22d6295bd650eb07d4f0
OlivierPuimege <olivier.puimege@student.uantwerpen.be>
2020-03-29 22:01:53 +0200

    Updat van 29/03

--- parallel_programmeren_project_olivier/__init__.py
+++ parallel_programmeren_project_olivier/__init__.py
@@ -5,7 +5,7 @@ Package parallel_programmeren_project_olivier
 
 A 'hello world' example.
 """
-__version__ = "0.1.6"
+__version__ = "0.3.2"
 
 
 def hello(who='world'): #Laten staan voor debug
--- parallel_programmeren_project_olivier/parallel_programmeren_project_olivier.py
+++ parallel_programmeren_project_olivier/parallel_programmeren_project_olivier.py
@@ -5,12 +5,8 @@ Package parallel_programmeren_project_olivier
 
 A 'hello world' example.
 """
-__version__ = "0.1.4"
 
-from Atoom import Atoom
-from VolledigeRuimte import VolledigeRuimte
-import math
-import numpy as np
+
 
 def hello(who='world'):  # Laten staan voor debug
     """'Hello world' method.
@@ -21,16 +17,6 @@ def hello(who='world'):  # Laten staan voor debug
     result = "Hello " + who
     return result
 
-testvariabele = Atoom(100,100,100)
-testvariabele.testfunctie()
-Atoom2 = Atoom(0,0,0)
-testje = VolledigeRuimte([],[])
-testje.toevoegenAtoom(testvariabele)
-testje.toevoegenAtoom(Atoom2)
-print("Hier zou ik alle x-coördinaten moeten uitprinten")
-testje.printDeXCoordinaten()
-testje.energieUpdaten()
-print("Klaar")
 
 
 # eof
